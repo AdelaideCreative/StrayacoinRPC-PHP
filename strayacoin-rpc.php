5f17b86c39284ad3ede86717ff74f814d4c9f2c2
AdelaideCreative <david@adelaidecreative.com.au>
2018-02-17 19:57:58 +1030

    Update strayacoin-rpc.php

--- strayacoin-rpc.php
+++ strayacoin-rpc.php
@@ -24,29 +24,29 @@ THE SOFTWARE.
 ====================
 */
 // Initialize StrayaCoin connection/object
-$strayacoin = new Strayacoin('username','password');
+//$strayacoin = new Strayacoin('username','password');
 // Optionally, you can specify a host and port.
-$strayacoin = new Strayacoin('username','password','host','port');
+//$strayacoin = new Strayacoin('username','password','host','port');
 // Defaults are:
 //	host = localhost
-//	port = 6969
+//	port = 9666
 //	proto = http
 // If you wish to make an SSL connection you can set an optional CA certificate or leave blank
 // This will set the protocol to HTTPS and some CURL flags
-$strayacoin->setSSL('/full/path/to/mycertificate.cert');
+//$strayacoin->setSSL('/full/path/to/mycertificate.cert');
 // Make calls to strayacoind as methods for your object. Responses are returned as an array.
 // Examples:
-$strayacoin->getinfo();
-$strayacoin->getrawtransaction('0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098',1);
-$strayacoin->getblock('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f');
+//$strayacoin->getinfo();
+//$strayacoin->getrawtransaction('0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098',1);
+//$strayacoin->getblock('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f');
 // The full response (not usually needed) is stored in $this->response while the raw JSON is stored in $this->raw_response
 // When a call fails for any reason, it will return FALSE and put the error message in $this->error
 // Example:
-echo $strayacoin->error;
+//echo $strayacoin->error;
 // The HTTP status code can be found in $this->status and will either be a valid HTTP status code or will be 0 if cURL was unable to connect.
 // Example:
-echo $strayacoin->status;
-*/
+//echo $strayacoin->status;
+
 class Strayacoin {
     // Configuration options
     private $username;
@@ -70,7 +70,7 @@ class Strayacoin {
      * @param string $proto
      * @param string $url
      */
-    function __construct($username, $password, $host = 'localhost', $port = 6969, $url = null) {
+    function __construct($username, $password, $host = 'localhost', $port = 9666, $url = null) {
         $this->username      = $username;
         $this->password      = $password;
         $this->host          = $host;
